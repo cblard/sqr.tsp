d1508b5acc138b850acda54e82550f7320fa4b7b
chris <chris@192.168.0.17>
2021-01-04 14:17:29 +0100

    BLARD Christopher
    Ajout des tests pour les fonctions distanceTo() de la classe City, et
    getDistance() de la classe Tour.

--- src/test/java/tsp/lee/jacobson/CityTest.java
+++ src/test/java/tsp/lee/jacobson/CityTest.java
@@ -5,10 +5,64 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CityTest {
+	
+	City ville1 = new City(0,0);
+	City ville2 = new City(1,0);
+	City ville3 = new City(0,1);
+	City ville3Negatif = new City(0,-1);
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	/**
+	 * GIVEN distanceTo
+	 * WHEN ville3, ville3
+	 * THEN 0
+	 */
+	public void distanceTo_ville3toville3_0(){
+		double expected = 0;
+		double actual = ville3.distanceTo(ville3);
+		assertEquals(expected, actual, 0.0);
+	}
+	
+	@Test
+	/**
+	 * GIVEN distanceTo
+	 * WHEN ville1, ville2
+	 * THEN 1
+	 */
+	public void distanceTo_ville1toville2_1(){
+		double expected = 1 ;
+		double actual = ville1.distanceTo(ville2);
+		assertEquals(expected, actual, 0.0);
+	}
+	
+	@Test
+	/**
+	 * GIVEN distanceTo
+	 * WHEN ville2, ville1
+	 * THEN 1
+	 */
+	public void distanceTo_ville2toville1_1() {
+		double expected = 1 ;
+		double actual = ville2.distanceTo(ville1);
+		assertEquals(expected, actual, 0.0);
+	}
+	
+	@Test
+	/**
+	 * GIVEN distanceTo
+	 * WHEN ville1, ville3Negatif
+	 * THEN 1
+	 */
+	public void distanceTo_ville1toville3Negatif_1() {
+		double expected = 1 ;
+		double actual = ville1.distanceTo(ville3Negatif);
+		assertEquals(expected, actual, 0.0);
 	}
 
+	@Test(expected=NullPointerException.class)
+	public void distanceTo_Null_NullPointerException() {
+		double actual = ville3.distanceTo(null);
+	}
+	
+	
 }
--- src/test/java/tsp/lee/jacobson/TourTest.java
+++ src/test/java/tsp/lee/jacobson/TourTest.java
@@ -3,12 +3,71 @@ package tsp.lee.jacobson;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+import java.util.ArrayList;
 public class TourTest {
 
+	ArrayList chemin = new ArrayList();
+	City ville1 = new City(0,0);
+	City ville2 = new City(1,0);
+	City ville3 = new City(0,1);
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	/**
+	 * GIVEN getDistance
+	 * WHEN le chemin est vide (aucune ville)
+	 * THEN 0
+	 */
+	public void getDistance_cheminNul_0() {
+		Tour tour = new Tour();
+		int expected = 0;
+		int actual = tour.getDistance();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	/**
+	 * GIVEN getDistance
+	 * WHEN le chemin ne contient qu'une ville
+	 * THEN 0
+	 */
+	public void getDistance_uneVille_0() {
+		chemin.add(ville1);
+		Tour tour = new Tour(chemin);
+		int expected = 0;
+		int actual = tour.getDistance();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	/**
+	 * GIVEN getDistance
+	 * WHEN le chemin contient deux villes
+	 * THEN 2
+	 */
+	public void getDistance_deuxVilles_0() {
+		chemin.add(ville1);
+		chemin.add(ville2);
+		Tour tour = new Tour(chemin);
+		int expected = 2;
+		int actual = tour.getDistance();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	/**
+	 * GIVEN getDistance
+	 * WHEN le chemin contient trois villes
+	 * THEN 3
+	 */
+	public void getDistance_troisVilles_0() {
+		chemin.add(ville1);
+		chemin.add(ville2);
+		chemin.add(ville3);
+		Tour tour = new Tour(chemin);
+		System.out.println(tour.getDistance());
+		int expected = 3;
+		int actual = tour.getDistance();
+		assertEquals(expected, actual);
 	}
 
 }
